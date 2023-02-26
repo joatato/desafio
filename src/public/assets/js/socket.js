d02fe9d5e5ba0960b42cea844b5c3605eba704f4
joatato <joaquintatoaguero@gmail.com>
2023-02-25 21:28:07 -0300

    limpieza

--- src/public/assets/js/socket.js
+++ src/public/assets/js/socket.js
@@ -2,18 +2,6 @@ let productMessage = document.querySelector('#mensajeDeActualizacion');
 let divActualizador = document.getElementById('actualizador')
 let socket = io();
 
-/* socket.on('newProduct', (product) => {
-    console.log('Esto andaaaa!!!');
-    productMessage.innerHTML = `<p> Se ha añadido un nuevo producto </p><b>${product.title}</b>`
-    divActualizador.innerHTML += `<li>
-    <b> ${product.title}</b>
-    <br>
-    Precio: ${product.price}
-    <br>
-    Descripción: ${product.description}
-    <br>
-    </li>`
-}) */
 socket.on('editProduct', async (products) => {
     console.log('Esto andaaaa!!!', products, products[0].title);
     divActualizador.innerHTML = ''
@@ -29,7 +17,5 @@ socket.on('editProduct', async (products) => {
     </li>`
     }
     productMessage.innerHTML = `<p> Se ha editado la lista de productos </p>`
-
-
 })
 
